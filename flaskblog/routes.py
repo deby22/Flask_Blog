56f78ec17a306838cdffc4cec86b32f7dc6529dc
Dawid Dęby <ddeby@totalbet.pl>
2021-11-04 23:30:29 +0100

    Part 6 - User Authentication - next-page after login

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 
 from flaskblog import app, bcrypt, db
 from flaskblog.forms import RegistrationForm, LoginForm
@@ -59,7 +59,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("home"))
+            next_page = request.args.get("next")
+            return redirect(next_page) or redirect(url_for("home"))
         else:
             flash("Something goes wrong. Please check email and password", "danger")
     return render_template("login.html", title="Register", form=form)
